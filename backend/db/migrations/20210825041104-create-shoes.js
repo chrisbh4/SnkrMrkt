9a8bf06669f733bfa4f13c41569af241e7fc9eb4
Chrisbh4 <chrismbh4@gmail.com>
2021-08-24 21:41:22 -0700

    Need to fix naming in Models

---
+++ backend/db/migrations/20210825041104-create-shoes.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Shoes', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{model :'Users'}
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      shoeSize: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      price: {
+        type: Sequelize.DECIMAL,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Shoes');
+  }
+};
---
+++ backend/db/migrations/20210825042647-create-review.js
@@ -0,0 +1,43 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Reviews', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      shoeId: {
+        type: Sequelize.INTEGER,
+        references:{model:"Shoes"},
+        nullable:false
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        references:{model:"Users"},
+        nullable:false
+      },
+      comment: {
+        type: Sequelize.STRING
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      image: {
+        type: Sequelize.TEXT
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Reviews');
+  }
+};
