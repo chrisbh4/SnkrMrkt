76f467fccca0d805f69db0b8194cadc9be88cf24
Chrisbh4 <chrismbh4@gmail.com>
2021-08-25 16:15:40 -0700

    3/5 shoe routes complete

--- backend/routes/api/shoes.js
+++ backend/routes/api/shoes.js
@@ -10,4 +10,19 @@ router.get('/', asyncHandler(async (req, res )=>{
     return res.send(allShoes)
 }));
 
+router.get('/:id', asyncHandler(async ( req , res )=>{
+    const shoe = await Shoes.findByPk(req.params.id)
+    return res.send(shoe)
+}))
+
+router.post('/new', asyncHandler(async ( req , res )=>{
+    const {sellerId , title, shoeSize, image, price} = req.body
+
+    const newShoe = await Shoes.create({
+        sellerId, title, shoeSize, image, price
+    })
+
+    return res.json({newShoe})
+}))
+
 module.exports = router
