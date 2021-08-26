ec7ddc0da0e40bfafa580af3732e4c646a9733b7
Chrisbh4 <chrismbh4@gmail.com>
2021-08-26 14:29:58 -0700

    Shoes store 3/4 done

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import shoes from './shoes'
 
 const rootReducer = combineReducers({
-  session
+  session,
+  shoes
 });
 
 let enhancer;
