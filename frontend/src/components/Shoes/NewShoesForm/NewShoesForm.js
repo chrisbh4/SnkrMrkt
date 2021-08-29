33ac109e3a8dc9e39de698234fe16c3cc9c652eb
Chrisbh4 <chrismbh4@gmail.com>
2021-08-28 19:18:20 -0700

    Create shoe form functionality complete

--- frontend/src/components/Shoes/NewShoesForm/NewShoesForm.js
+++ frontend/src/components/Shoes/NewShoesForm/NewShoesForm.js
@@ -1,8 +1,8 @@
-import React , from "react"
+import React from "react"
 import { useState } from "react"
-import { useSelector , useDispatch} from "react-redux"
-import { getCreatedShoe} from "../../../store/shoes"
-import {useHistory} from "react-router-dom"
+import { useSelector, useDispatch } from "react-redux"
+import {getCreatedShoe} from "../../../store/shoes.js"
+import { useHistory } from "react-router-dom"
 
 
 
@@ -10,21 +10,35 @@ import {useHistory} from "react-router-dom"
 function NewShoesForm() {
     const dispatch = useDispatch()
     const history = useHistory()
-    const user = useSelector((state) => state.session.user.id)
+
+    const sellerId = useSelector((state) => state.session.user.id)
     // console.log('User Id: ',user)
-    
+
+    const [title, setTitle] = useState("")
+    const [shoeSize, setShoeSize] = useState(6)
+    const [image, setImage] = useState("")
+    const [brand, setBrand] = useState("")
+    const [price, setPrice] = useState(0.00)
+
+    const updateTitle = (e) => setTitle(e.target.value)
+    const updateShoeSize = (e) => setShoeSize(e.target.value)
+    const updateImage = (e) => setImage(e.target.value)
+    const updateBrand = (e) => setBrand(e.target.value)
+    const updatePrice = (e) => setPrice(e.target.value)
 
 
-    const onSubmit = async (e)=>{
+    const onSubmit = async (e) => {
         e.preventDefault();
-        const data = await dispatch(getCreatedShoe())
+        const data = await dispatch(getCreatedShoe(sellerId,title,shoeSize,image,price))
+            // brand,
 
-        if(data.errors){
-            history('/')
+        if (!data.errors) {
+            history.push('/')
         }
-        else{
+        else {
             // setErros(data)
         }
+        return data
     }
 
     return (
@@ -34,7 +48,7 @@ function NewShoesForm() {
                     <label>Shoe Title: </label>
                     <input
                         type="text"
-
+                        onChange={updateTitle}
                         name="title"
                         required
                     ></input>
@@ -43,7 +57,7 @@ function NewShoesForm() {
                     <label>ShoeSize</label>
                     <input
                         type="number"
-
+                        onChange={updateShoeSize}
                     ></input>
                 </div>
                 <div>
@@ -53,24 +67,28 @@ function NewShoesForm() {
                     <input
                         type="radio"
                         value="Air-Jordan"
+                        onChange={updateBrand}
                         name="brand"
                     ></input>
                     <label for="">Air Jordan</label>
                     <input
                         type="radio"
                         value="Nike"
+                        onChange={updateBrand}
                         name="brand"
                     ></input>
                     <label>Nike</label>
                     <input
                         type="radio"
                         value="Yeezy-Adidas"
+                        onChange={updateBrand}
                         name="brand"
                     ></input>
                     <label>Yeezy-Adidas</label>
                     <input
                         type="radio"
                         value="Adidas"
+                        onChange={updateBrand}
                         name="brand"
                     ></input>
                     <label>Adidas-Original</label>
@@ -79,13 +97,16 @@ function NewShoesForm() {
                     <label>Price: $</label>
                     <input
                         type="number"
-
+                        onChange={updatePrice}
                     ></input>
                 </div>
-
-
-
-
+                <div>
+                    <label>Image Url</label>
+                    <input
+                        type="text"
+                        onChange={updateImage}
+                    ></input>
+                </div>
 
                 <div className="button-containers">
                     <button>Submit New Listing</button>
