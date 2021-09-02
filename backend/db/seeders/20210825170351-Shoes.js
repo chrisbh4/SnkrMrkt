4c75abf72f222239aadbf6d0ceaabebb11a51ba9
Chrisbh4 <chrismbh4@gmail.com>
2021-09-02 14:15:17 -0700

    added more test seeders for shoes

--- backend/db/seeders/20210825170351-Shoes.js
+++ backend/db/seeders/20210825170351-Shoes.js
@@ -11,19 +11,19 @@ module.exports = {
       {sellerId:1, title:'test1', shoeSize:3, image:null ,price:33.30, createdAt: new Date(), updatedAt:new Date()},
       {sellerId:1, title:'test2', shoeSize:3, image:null ,price:33.30, createdAt: new Date(), updatedAt:new Date()},
       {sellerId:1, title:'test3', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      {sellerId:1, title:'test4', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()}
-      // {sellerId:1, title:'test5', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test6', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:1, title:'test7', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
-      // {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:44.42, createdAt: new Date(), updatedAt:new Date()}
+      {sellerId:1, title:'test4', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test5', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test6', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test8', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test9', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test10', shoeSize:3, image:null ,price:44.41, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:1, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()},
+      {sellerId:2, title:'test-0', shoeSize:3, image:null ,price:450.00, createdAt: new Date(), updatedAt:new Date()}
     ], {});
   },
 
