a4fcfec5c6364d924ae880e96fe2246301d65200
Chrisbh4 <chrismbh4@gmail.com>
2021-09-09 19:57:38 -0700

    review grid built

--- frontend/src/components/Reviews/ShoeReviews/ShoeReviews.js
+++ frontend/src/components/Reviews/ShoeReviews/ShoeReviews.js
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 import "./ShoeReviews.css"
 
 
-function ShoeReviews({shoe}){
+function ShoeReviews({ shoe }) {
 
     const userId = useSelector((state) => {
         if (state.session.user) {
@@ -13,50 +13,57 @@ function ShoeReviews({shoe}){
     })
 
     let createReviewButton;
-    if(userId > 0.9){
+    if (userId > 0.9) {
         createReviewButton = (
             <>
-            <button className="leave-review-button"><a href={`/shoes/${shoe?.id}/reviews/new`}>Leave a Review</a></button>
-            <div className="review-labels">
-                <p>Review</p>
-                <p> Created By</p>
-                <p> Review Image</p>
-            </div>
+                <button className="leave-review-button"><a href={`/shoes/${shoe?.id}/reviews/new`}>Leave a Review</a></button>
+                <div className="review-labels">
+                    <p className='label-review-image'> Review Image</p>
+                    <p className="label-review-comment">Review</p>
+                    <p className="label-review-username"> Username</p>
+                </div>
             </>
         )
     }
 
     const reviewsAndEditButton = shoe?.Reviews.map((review) => {
         // console.log("single review:", review.id)
-        if ( userId === review.userId){
-            return(
+        if (userId === review.userId) {
+            return (
                 <div className="review-container">
-            <p>{review.comment} </p>
-            <p>Reviewd by: 'UserName'</p>
-            <img className="review-image" src={review.image} alt={"shoe"} />
-           <button > <a href={`/reviews/${review.id}/edit`}>Edit</a></button>
-            </div>
-            )
-        }else{
-            return <div>
-                <p>
-                {review.comment}
-                </p>
-                <p>{review.userId}</p>
-                <img src={review.image} alt="user review"></img>
+                    <button className="review-edit-button"> <a href={`/reviews/${review.id}/edit`}>Edit</a></button>
+                    <p className="review-comment">
+                        {review.comment}
+                    </p>
+                    <p className="review-usernane">
+                        {review.userId}
+                    </p>
+                    <img src={review.image} alt="user review" className="review-image"></img>
+
                 </div>
+            )
+        } else {
+            return <div className="review-container">
+                <p className="review-comment">
+                    {review.comment}
+                </p>
+                <p className="review-username">
+                    {review.userId}
+                </p>
+                <img src={review.image} alt="user review" className="review-image"></img>
+            </div>
 
         }
-   })
+    })
 
-    return(
+    return (
         <>
-        {/* <h3>Reviews</h3> */}
-        {createReviewButton}
-        <div>
-        {reviewsAndEditButton}
-        </div>
-    </>
+            {/* <h3>Reviews</h3> */}
+            {createReviewButton}
+            <div>
+                {reviewsAndEditButton}
+            </div>
+        </>
     )
 }
 
