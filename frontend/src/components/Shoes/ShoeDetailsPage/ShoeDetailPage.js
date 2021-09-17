6830f53c7ee0591fdefac86b2d9f7c5ec9fd25e9
Chrisbh4 <chrismbh4@gmail.com>
2021-09-16 18:34:28 -0700

    redirect added to all titles

--- frontend/src/components/Shoes/ShoeDetailsPage/ShoeDetailPage.js
+++ frontend/src/components/Shoes/ShoeDetailsPage/ShoeDetailPage.js
@@ -92,7 +92,9 @@ function ShoesDetailsPage() {
             {/* Reviews is outside of the ' detials-container ' */}
             {/* Take all of this and place inside a componenet and pass in the shoe as a prop */}
             {/* <div className="reviews-title"> */}
-            <h2 className="reviews-title">Reviews</h2>
+            <h2 id="reviews-title">
+                <a href="/">Reviews</a>
+            </h2>
             {/* </div> */}
 
             <div className="reviews-container">
