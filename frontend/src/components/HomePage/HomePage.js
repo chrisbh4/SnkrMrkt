2532a0f1c06cf7181d399e6412acca488fb12baf
Chrisbh4 <chrismbh4@gmail.com>
2021-09-16 18:36:30 -0700

    clean up

--- frontend/src/components/HomePage/HomePage.js
+++ frontend/src/components/HomePage/HomePage.js
@@ -1,5 +1,4 @@
 import React  ,{useEffect} from "react";
-import {Link} from "react-router-dom"
 import { useSelector, useDispatch } from "react-redux";
 import { getAllShoes} from "../../store/shoes";
 import './ShoeList.css'
