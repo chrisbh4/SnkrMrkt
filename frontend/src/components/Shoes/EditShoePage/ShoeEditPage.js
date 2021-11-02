b56762b544acb4715cc3590f2a0f4b51be4fa1a4
Chrisbh4 <chrismbh4@gmail.com>
2021-11-01 17:19:02 -0700

    homepage title removed and edit shoe redirect fixed to go to homepage

--- frontend/src/components/Shoes/EditShoePage/ShoeEditPage.js
+++ frontend/src/components/Shoes/EditShoePage/ShoeEditPage.js
@@ -72,7 +72,7 @@ function EditShoesForm() {
 
         await dispatch(getDeletedShoe(shoe.id))
         alert("Shoe has been deleted.");
-        history.push('/')
+        history.push('/home')
     }
 
     return (
