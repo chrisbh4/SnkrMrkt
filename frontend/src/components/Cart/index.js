a01355cb129e6950bdc230e08e7a9b2f5fbf9832
Christian Brown <chrismbh4@gmail.com>
2022-09-02 22:25:47 -0700

    Shopping cart details info updated

--- frontend/src/components/Cart/index.js
+++ frontend/src/components/Cart/index.js
@@ -6,6 +6,17 @@ import CartItem from "./CartItem";
 import "./Cart.css"
 
 
+/*
+ * Chakra UI components
+Img
+- https://chakra-ui.com/docs/components/image/usage
+
+Container
+-
+
+
+*/
+
 
 
 function ShoppingCart() {
@@ -22,10 +33,10 @@ function ShoppingCart() {
     cart.forEach((item) => {
         total += parseFloat(item.price)
     })
-    const totalPrice = total.toFixed(2)
+    const totalPriceOfShoes = total.toFixed(2)
 
-    // console.log("total", totalPrice);
-    const emptyCart = <h2 className="empty-cart">Cart is empty 😢 </h2>
+    // console.log("total", totalPriceOfShoes);
+    const emptyCart = <h1 className="empty-cart">Shoppping Cart is empty </h1>
 
     const purchaseTheCart = async () => {
         await dispatch(purchaseFromCart())
@@ -41,16 +52,18 @@ function ShoppingCart() {
 
         <div className="cart-placement">
             <div className="cart-container">
-                <h1 className="cart-title" ><a id="user-title" href="/home">{userTitle}'s Cart</a></h1>
                 {cart.map((item) => (
                     <CartItem item={item} key={item.id} />
                 ))}
             </div>
             <div className="cart-info">
-                <h2 className="total-price">{totalPrice > 0 ? `Total Amount: $${totalPrice}` : emptyCart}</h2>
+                <h2 className="total-price">{totalPriceOfShoes > 0 ? `Shoes : $${totalPriceOfShoes}` : emptyCart}</h2>
+                <h2 className="total-price">{totalPriceOfShoes > 0 ? `Site fee 1.5%  : $1.50` : null}</h2>
+                <h2 className="total-price">{totalPriceOfShoes > 0 ? `Sales tax: $2.00` : null}</h2>
+                <h2 className="total-price">{totalPriceOfShoes > 0 ? `Total: $${totalPriceOfShoes + 3}` : null}</h2>
 
 
-                {totalPrice > 0 ? <button className="cart-purchase-button" onClick={purchaseTheCart} >Purchase</button> : null}
+                {totalPriceOfShoes > 0 ? <button className="cart-purchase-button" onClick={purchaseTheCart} >Purchase</button> : null}
                 <div className="cart-footer-placement">
 
 
