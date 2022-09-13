a3effecceddd846723ac4ed016c37534242510b4
Christian Brown <chrismbh4@gmail.com>
2022-09-13 11:37:44 -0700

    Home page section containers created

--- frontend/src/components/NewHome/NewHome.js
+++ frontend/src/components/NewHome/NewHome.js
@@ -1,67 +1,101 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Grid, Center, GridItem, Box, VStack, StackDivider, Text, Flex } from '@chakra-ui/react'
 import { useSelector, useDispatch } from "react-redux";
 import Navigation from "../Navigation/index";
 import { getAllShoes } from "../../store/shoes";
 import ShoeList from "../HomePage/ShoeList";
 
- function NewHomePage(){
+function NewHomePage() {
 
-    const dispatch = useDispatch()
-    const shoes= useSelector((state)=> state.shoes)
-    console.log("shoes :", shoes)
-    const shoesArray = Object.values(shoes)
+  const dispatch = useDispatch()
+  const shoes = useSelector((state) => state.shoes)
+  console.log("shoes :", shoes)
+  const shoesArray = Object.values(shoes)
 
-    useEffect(()=>{
-        dispatch(getAllShoes())
-    },[dispatch])
+  useEffect(() => {
+    dispatch(getAllShoes())
+  }, [dispatch])
 
 
-    return (
+  return (
     <>
+      <VStack
+        divider={<StackDivider borderColor='gray.200' />}
+        spacing={4}
+        align='stretch'
+        py='5%'>
 
-
-    <VStack
-  divider={<StackDivider borderColor='gray.200' />}
-  spacing={4}
-  align='stretch'
-  py='5%'
->
-  <Box h='40px' bg='yellow.200'>
-    <Center>
-    <Text fontSize="2xl">Just released</Text>
-
-    </Center>
-  </Box>
-  <Box h='40px' bg='tomato'>
-    <Center>
-    <Text fontSize="2xl">Best Sellers</Text>
-    </Center>
-  </Box>
-  <Box h='40px' bg='pink.100'>
-    <Center>
-    <Text fontSize={"2xl"}>Price Drops</Text>
-    </Center>
-  </Box>
-  <Box h='40px' bg='blue.400'>
-    <Center>
-    <Text fontSize={"2xl"}>Best Sellers</Text>
-    </Center>
-  </Box>
-
-  <Flex
-    w="50%"
-  >
-  {shoesArray.map((shoe)=>{
-                return(
-                    <div className="shoe-container" key={shoe.id}>
-                        <ShoeList shoe={shoe} key={shoe.id} />
-
-                </div>
+        <Box h="20%" bg='yellow.200'>
+          <VStack>
+            <Text fontSize="2xl">Just released</Text>
+            <Flex
+              w="100%"
+              overflow="scroll" >
+              {shoesArray.map((shoe) => {
+                return (
+                  <div className="shoe-container" key={shoe.id}>
+                    <ShoeList shoe={shoe} key={shoe.id} />
+                  </div>
                 )
-    })}
-  </Flex>
-</VStack>
+              })}
+            </Flex>
+          </VStack>
+        </Box>
+
+        <Box  bg='tomato'   >
+          <VStack>
+            <Text fontSize="2xl">Most Popular</Text>
+            <Flex
+              w="100%"
+              overflow="scroll" >
+              {shoesArray.map((shoe) => {
+                return (
+                  <div className="shoe-container" key={shoe.id}>
+                    <ShoeList shoe={shoe} key={shoe.id} />
+                  </div>
+                )
+              })}
+            </Flex>
+          </VStack>
+        </Box>
+
+        <Box  bg='blue.400'>
+          <VStack>
+            <Text fontSize={"2xl"}>Best Sellers</Text>
+            <Flex
+              w="100%"
+              overflow="scroll" >
+              {shoesArray.map((shoe) => {
+                return (
+                  <div className="shoe-container" key={shoe.id}>
+                    <ShoeList shoe={shoe} key={shoe.id} />
+                  </div>
+                )
+              })}
+            </Flex>
+          </VStack>
+        </Box>
+
+        <Box  bg='pink.100'>
+          <VStack>
+            <Text fontSize={"2xl"}>Brands</Text>
+            <Flex
+              w="100%"
+              overflow="scroll" >
+              {shoesArray.map((shoe) => {
+                return (
+                  <div className="shoe-container" key={shoe.id}>
+                    <ShoeList shoe={shoe} key={shoe.id} />
+                  </div>
+                )
+              })}
+            </Flex>
+          </VStack>
+        </Box>
+
+
+
+      </VStack>
     </>)
 }
 
