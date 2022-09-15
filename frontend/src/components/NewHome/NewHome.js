1d9db101fa2aa7ca51b105c6b0ea4d5b4497bd0c
Christian Brown <chrismbh4@gmail.com>
2022-09-15 16:12:26 -0700

    Shoe Filter titles set to the left , Prices switched to checkboxes , Checkboxes border-color & colorScheme set

--- frontend/src/components/NewHome/NewHome.js
+++ frontend/src/components/NewHome/NewHome.js
@@ -19,75 +19,80 @@ function NewHomePage() {
   return (
     <>
       <Grid
+        pl='3px'
         pt="2%"
         templateRows='repeat(2, 1fr)'
         templateColumns='repeat(5, 1fr)'
-        gap={4}
+
         // h='auto'
         h='1150px'
         overflow='scroll'
-      >
+        >
         {/* Shoe Filter Nav */}
         <GridItem
           rowSpan={2}
           colSpan={1}
-          bg='brown'
+
           overflow={'scroll'}
+          w='75%'
+          pl='2%'
 
         >
 
-          <Box  pt='3%' pb='2%'>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Yeezy</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Air Jordan</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Adidas</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Nike</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >New Balance</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Reebok</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >converse</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >PUma</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >vans</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Collections</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Luxury Brands</Center>
-            <Center fontSize='24px' color='white' textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.200" }} >Other Brands</Center>
+          <Box pl='1%'  pt='3%' pb='2%'>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Yeezy</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Air Jordan</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Adidas</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Nike</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >New Balance</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Reebok</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >converse</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >PUma</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >vans</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Collections</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Luxury Brands</Text>
+            <Text textAlign={'left'} fontSize='24px'  textTransform={"uppercase"} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Other Brands</Text>
           </Box>
 
-          <Box border={'solid'}   >
-            <Center fontSize='30px' color='white'>Shoe Style</Center>
-            <VStack align={'start'} position='relative' left={'40%'} pb='3%' pt='2%' >
-              <Checkbox><Text fontSize={'20px'} textTransform='uppercase'>Men</Text></Checkbox>
-              <Checkbox><Text fontSize={'20px'} textTransform='uppercase'>Woman</Text></Checkbox>
-              <Checkbox><Text fontSize={'20px'} textTransform='uppercase'>Child</Text></Checkbox>
-              <Checkbox><Text fontSize={'20px'} textTransform='uppercase'>Toddler</Text></Checkbox>
+          <Box borderTop={'2px'}   >
+            <Text fontSize='30px'  position='relative' left='1%'>Shoe Style</Text>
+            <VStack align={'start'} position='relative' left={'7.5%'} pb='3%' pt='2%' >
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} ><Text fontSize={'20px'} textTransform='uppercase' >Men</Text></Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} ><Text fontSize={'20px'} textTransform='uppercase' >Woman</Text></Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} ><Text fontSize={'20px'} textTransform='uppercase' >Child</Text></Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} ><Text fontSize={'20px'} textTransform='uppercase' >Toddler</Text></Checkbox>
             </VStack>
           </Box>
 
-          <Box border={"solid"}  >
-            <Center fontSize='30px' color='white'>Shoe Size</Center>
+          <Box borderTop={"2px"}  >
+            <Text fontSize='30px' pl='2%'>Shoe Size</Text>
+          <Flex pl='2%'>
             <Center>
             <SimpleGrid columns={4} rows={5} pt='3%' pb='5%' spacingY='9px'>
               {shoeSizeChart.map((size) => {
                 return (
                   <>
-                      <Button w='0%' _hover={{bg: "gray.300"}}>{size}</Button>
+                      <Button w='0%'bg='gray.400' _hover={{bg: "gray.100", border:"2px"}}>{size}</Button>
                   </>
                 )
               })}
             </SimpleGrid>
 
               </Center>
+              </Flex>
           </Box>
 
-          <Box border={'solid'} pb='15px'>
-            <Center fontSize='30px' color='white'>Prices</Center>
-            <Center pt='3%'>
-            <SimpleGrid columns={2}  spacingY='9px'justifyContent={'center'} >
-              <Button w='90%' px={'9px'} _hover={{bg: "gray.300"}}   >$100 & under</Button>
-              <Button w='90%' _hover={{bg: "gray.300"}} >$200-300</Button>
-              <Button w='90%' _hover={{bg: "gray.300"}} >$300-400</Button>
-              <Button w='90%' _hover={{bg: "gray.300"}} >$500-650</Button>
-              <Button w='90%' _hover={{bg: "gray.300"}} >$650 +</Button>
-            </SimpleGrid>
-            </Center>
+          <Box borderTop={'solid'} pb='15px'>
+            <Text pl='2%' fontSize='30px' >Prices</Text>
+            <Box position='relative' left={'7.5%'} pt='3%'>
+            <VStack columns={2}  spacingY='9px'justifyContent={'center'} >
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", bg: "gray.300" }} >$100 & under</Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", bg: "gray.300" }} >$200-300</Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", bg: "gray.300" }} >$300-400</Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", bg: "gray.300" }} >$500-650</Checkbox>
+              <Checkbox size={'lg'} w='100%' position='relative' right='6%' borderColor={'black'} colorScheme='red' _hover={{ color: "black", bg: "gray.300" }} >$650 +</Checkbox>
+            </VStack>
+            </Box>
           </Box>
 
 
