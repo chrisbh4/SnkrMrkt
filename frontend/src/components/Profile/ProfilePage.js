2dd9635b9cab19be0499367abc6f186faa23fa3e
Christian Brown <chrismbh4@gmail.com>
2022-09-17 21:35:41 -0700

    Profile component basic layout created

--- frontend/src/components/Profile/ProfilePage.js
+++ frontend/src/components/Profile/ProfilePage.js
@@ -1,5 +1,5 @@
-import { Box, Button, Center, Image, VStack, Link, Flex, Text, SimpleGrid, GridItem } from "@chakra-ui/react";
 import React from "react";
+import { Box, Button, Center, Image, VStack, Link, Flex, Text, SimpleGrid, GridItem, Wrap, WrapItem, Avatar } from "@chakra-ui/react";
 
 
 
@@ -9,23 +9,53 @@ function ProfilePage() {
   return (
     <Box pos='relative' top='50px'>
       <Flex h='1000px'    >
-        <Box w='40%' bg='gray.300'>
-          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text fontSize={'xl'}   >Profile</Text></Flex>
-          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text fontSize={'xl'} pb='3%' >Settings</Text></Flex>
-          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text fontSize={'xl'} pb='3%' >Buying</Text></Flex>
-          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text fontSize={'xl'} pb='3%' >Selling</Text></Flex>
-          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text fontSize={'xl'} pb='3%' >Following</Text></Flex>
+        <Box w='20%' bg=''>
+          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text w='100%' fontSize={'xl'} _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }}  >Profile</Text></Flex>
+          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text w='100%' fontSize={'xl'} pb='3%' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Settings</Text></Flex>
+          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text w='100%' fontSize={'xl'} pb='3%' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Buying</Text></Flex>
+          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text w='100%' fontSize={'xl'} pb='3%' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Selling</Text></Flex>
+          <Flex alignItems={'center'} h='70px' borderBottom={'2px'}><Text w='100%' fontSize={'xl'} pb='3%' _hover={{ color: "black", fontWeight: "600", bg: "gray.300" }} >Following</Text></Flex>
         </Box>
-        <Box w='100%' bg='gray.300'>
+        <Box w='100%' bg=''>
 
-          <SimpleGrid columns={3} spacing={10} px='5%' pt='4%'>
-            <Box bg='white' height='80px'>Name</Box>
-            <Box bg='white' height='80px'>Shoe Size</Box>
-            <Box bg='white' height='80px'>Email </Box>
-            <Box bg='white' height='80px'>Username</Box>
-            <Box bg='white' height='80px'>Phone Number</Box>
-            <Box bg='white' height='80px'><Button bg='red.400'>Reset Password</Button></Box>
-          </SimpleGrid>
+          <Box>
+            <Flex justify={'space-between'} w='75%' py='8px' pos='relative' left='5%' borderBottom={'2px'}>
+              <Text fontSize={'30px'}>Profile</Text>
+              <Button bg='red.300'>Edit</Button>
+            </Flex>
+
+            <Wrap pl='6%' pt='10px'>
+              <WrapItem>
+                <Avatar size='2xl' name='Segun Adebayo' src='https://bit.ly/sage-adebayo' />{' '}
+              </WrapItem>
+            </Wrap>
+
+
+            <SimpleGrid columns={3} spacing={10} px='5%' pt='2%'>
+              <Box height='80px'>
+                <Text >Name :</Text>
+                <Text >Christian Brown</Text>
+              </Box>
+              <Box height='80px'>
+                <Text>Shoe Size :</Text>
+                <Text>13 M</Text>
+              </Box>
+              <Box height='80px'>
+                <Text>Email :</Text>
+                <Text>chris@gmail.com</Text>
+              </Box>
+              <Box height='80px'>
+                <Text>Username :</Text>
+                <Text>Chris_da_creator</Text>
+              </Box>
+              <Box height='80px'>
+                <Text>Phone Number :</Text>
+                <Text>333-444-5555</Text>
+              </Box>
+              <Box height='80px'><Button bg='red.400'>Reset Password</Button></Box>
+            </SimpleGrid>
+
+          </Box>
         </Box>
       </Flex>
     </Box>
