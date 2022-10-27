b1d3b089858db83bfd285c257752df5487e9e039
Christian Brown <chrismbh4@gmail.com>
2022-10-26 21:28:16 -0700

    Login form styling updated

--- frontend/src/components/LoginFormModal/LoginForm.js
+++ frontend/src/components/LoginFormModal/LoginForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as sessionActions from "../../store/session";
 import { useDispatch } from "react-redux";
-import { Grid, Center, GridItem, Box, VStack, Checkbox, Button, Text, Flex, SimpleGrid, Link, Input, Stack } from '@chakra-ui/react'
+import { Grid, Center, GridItem, Box, VStack, Checkbox, Button, Text, Flex, SimpleGrid, Link, Input, Stack, Form, FormControl, FormLabel, FormErrorMessage, FormHelperText, } from '@chakra-ui/react'
 import "./LoginForm.css";
 
 function LoginForm() {
@@ -15,6 +15,7 @@ function LoginForm() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    console.log("Inside Handle Submit")
 
     const data = await dispatch(sessionActions.login({ credential, password }))
 
@@ -41,66 +42,62 @@ function LoginForm() {
   // };
 
   return (
-    <div>
-      <h1 id="login-header">Log In</h1>
-      <form className="login-form" onSubmit={handleSubmit}>
-        {/* <ul> */}
-        {/* //! Cant console.log for some reason it unHighlights idx
-           might need to make my own errors.map */}
-        {errors.map((error, idx) => {
-          return <p key={idx}>{error}</p>
-        }
-        )}
-        {/* </ul> */}
-        <label>
-          Email:
-          <Input
-            variant='flushed'
-            placeholder='Enter Email'
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-            borderBottom={'2px'}
-            borderColor='black'
-            width={'full'}
-            />
-        </label>
+    <Box p='30px'>
+      <Center fontSize={'xl'} fontWeight='bold'>Log In </Center>
+      <FormControl onSubmit={handleSubmit}  >
+      {/* Error Handler */}
+        {/* <Box>
+          {errors.map((error, idx) => {
+            return <Text key={idx}>{error}</Text>
+          }
+          )}
+        </Box> */}
 
-        <label>
-          Password:
-            <Input
-              variant='flushed'
-              placeholder='Enter Password'
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-              borderBottom={'2px'}
-              borderColor='black'
-              width={'88%'}  />
-        </label>
-        {/* <button className="login-submit" type="submit">Log In</button> */}
-        <Button  marginRight={'20px'} type="submit">Log In</Button>
-      </form>
-      <div>
-        <footer>
-          <p>Christian Brown</p>
-          <div class="networking">
-            <a href="https://www.linkedin.com/in/christian-brown-8770311ba/">
-              <i class="fab fa-linkedin"></i>
-            </a>
-            <a href="mailto:Chrismbh4@gmail.com">
-              <i class="fas fa-envelope-square"></i>
-            </a>
+        <FormLabel >Email</FormLabel>
+        <Input
+          variant='flushed'
+          placeholder='Enter Email'
+          type="text"
+          id="email"
+          value={credential}
+          onChange={(e) => setCredential(e.target.value)}
+          required
+          borderBottom={'2px'}
+          borderColor='black'
+          width={'full'}
+        />
+        <FormLabel>Password</FormLabel>
+        <Input
+          variant='flushed'
+          placeholder='Enter Password'
+          type="password"
+          id='password'
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          borderBottom={'2px'}
+          borderColor='black'
+          width={'full'} />
 
-            <a href="https://github.com/chrisbh4">
-              <i class="fab fa-github"></i>
-            </a>
-          </div>
-        </footer>
-      </div>
-    </div>
+        <Button onSubmit={handleSubmit} type="submit" marginRight={'20px'}>Log In</Button>
+      </FormControl>
+
+      <footer>
+        <p>Christian Brown</p>
+        <div class="networking">
+          <a href="https://www.linkedin.com/in/christian-brown-8770311ba/">
+            <i class="fab fa-linkedin"></i>
+          </a>
+          <a href="mailto:Chrismbh4@gmail.com">
+            <i class="fas fa-envelope-square"></i>
+          </a>
+
+          <a href="https://github.com/chrisbh4">
+            <i class="fab fa-github"></i>
+          </a>
+        </div>
+      </footer>
+    </Box>
   );
 }
 
