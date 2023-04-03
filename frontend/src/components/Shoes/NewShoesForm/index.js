65f8620cdf86c461aa08504c4d85358016cd770c
Christian Brown <chrismbh4@gmail.com>
2023-04-02 22:34:33 -0700

    Form layout & styling added

--- frontend/src/components/Shoes/NewShoesForm/index.js
+++ frontend/src/components/Shoes/NewShoesForm/index.js
@@ -15,12 +15,15 @@ import {
     Box,
     Input,
     InputGroup,
+    InputLeftAddon,
     Heading,
     Text,
     Grid,
     Select,
     VStack,
-    Flex
+    Flex,
+    Button,
+    Textarea
 } from '@chakra-ui/react'
 
 
@@ -29,25 +32,25 @@ function NewShoesForm() {
     const dispatch = useDispatch()
     const navigate = useNavigate()
 
-    // const sellerId = useSelector((state) => state.session.user.id)
+    const sellerId = useSelector((state) => state.session.user.id)
 
-    // const [title, setTitle] = useState("")
-    // const [shoeSize, setShoeSize] = useState(0)
-    // // const [image, setImage] = useState("")
-    // const [imageFile, setImageFile] = useState("")
-    // const [brand, setBrand] = useState("")
-    // const [description , setDescription] = useState("")
+    const [title, setTitle] = useState("")
+    const [shoeSize, setShoeSize] = useState(0)
+    // const [image, setImage] = useState("")
+    const [imageFile, setImageFile] = useState("")
+    const [brand, setBrand] = useState("")
+    const [description, setDescription] = useState("")
 
-    // const [price, setPrice] = useState(0.00)
-    // const [errors, setErrors] = useState([]);
+    const [price, setPrice] = useState(0.00)
+    const [errors, setErrors] = useState([]);
 
-    // const updateTitle = (e) => setTitle(e.target.value)
-    // const updateShoeSize = (e) => setShoeSize(e.target.value)
-    // // const updateImage = (e) => setImage(e.target.value)
-    // const updateImageFile = (e) => setImageFile(e.target.files[0])
-    // const updateBrand = (e) => setBrand(e.target.value)
-    // const updatePrice = (e) => setPrice(e.target.value)
-    // const updateDescription= (e) => setDescription(e.target.value)
+    const updateTitle = (e) => setTitle(e.target.value)
+    const updateShoeSize = (e) => setShoeSize(e.target.value)
+    // const updateImage = (e) => setImage(e.target.value)
+    const updateImageFile = (e) => setImageFile(e.target.files[0])
+    const updateBrand = (e) => setBrand(e.target.value)
+    const updatePrice = (e) => setPrice(e.target.value)
+    const updateDescription = (e) => setDescription(e.target.value)
 
 
     // const onSubmit = async (e) => {
@@ -70,43 +73,51 @@ function NewShoesForm() {
 
     return (
         <>
-            <FormControl  >
-                <Box borderBottom="1px" borderColor="gray.100" pb={12} px='25%' >
-                    <Heading size="md" fontWeight="semibold" color="gray.900">Personal Information</Heading>
+            <FormControl pt={"2%"} bg={"gray.400"} >
+                <Box pb={12} px='25%'  >
+                    <Heading size="lg" fontWeight="semibold" color="gray.900" ml={"4%"}>Personal Information</Heading>
                     <Grid
                         templateRows="repeat(5, 1fr)"
                         templateColumns="repeat(1, 1fr)"
                         gap={4} // optional gap between grid items
-                        bg={"gray.400"}
+                        // bg={"gray.300"}
                         p={"4%"}
+                        borderBottom={"1px"}
+                        borderColor={"gray.500"}
                     >
-                        <Flex h={'20'} bg="blue.500" justify={'start'}>
-                            <Box w={'30%'}>
-                                <FormLabel>First Name</FormLabel>
-                                <Input bg='gray.200' placeholder="First Name" />
+                        <Flex h={'20'} justify={'start'}>
+                            <Box w={'40%'}>
+                                <FormLabel>Shoe Title</FormLabel>
+                                <Input bg='gray.50' placeholder="Shoe Title" />
                             </Box>
-                            <Box w={'30%'}>
-                                <FormLabel ml={'6%'}>Last Name</FormLabel>
-                                <Input  bg='gray.200' placeholder="Last Name" ml={'6%'} />
+                            <Box w={'40%'}>
+                                <FormLabel ml={'6%'}>Shoe Size</FormLabel>
+                                <Input bg='gray.50' placeholder="Shoe Size" ml={'6%'} />
                             </Box>
                         </Flex>
-                        <Box h={'20'} bg="green.500">
-                            <Input w={'30%'} bg='gray.200' placeholder="Email" />
+                        <Box h={'20'} w={"70%"}>
+                            <FormLabel>Description</FormLabel>
+                            <Textarea bg='gray.50' h={"90px"} placeholder="Description" />
                         </Box>
-                        <Box h={'20'} bg="red.500">
-                            <Input w={'30%'} bg='gray.200' placeholder="Country" />
+                        <Box h={'20'} w={"35%"} mt={"2%"} >
+                            <FormLabel>Brand</FormLabel>
+                            <Input bg='gray.50' placeholder="Brand" />
                         </Box>
-                        <Box h={'20'} bg="purple.500">
-                            <Input w={'30%'} bg='gray.200' placeholder="Street Address" />
+                        <Box h={'20'} w={"35%"} mt={"2%"} >
+                            <FormLabel>Price</FormLabel>
+                            <InputGroup>
+                                <InputLeftAddon children='$' />
+                                <Input bg='gray.50' placeholder="Price" />
+                            </InputGroup>
                         </Box>
-                        <Flex h={'20'} bg="blue.500" justify={'start'}>
-                            <Input w={'15%'} bg='gray.200' placeholder="City" />
-                            <Input w={'15%'} bg='gray.200' placeholder="State" ml={'6%'} />
-                            <Input w={'15%'} bg='gray.200' placeholder="Zip" ml={'6%'} />
-                        </Flex>
+                        <Box h={'20'} w={"50%"} >
+                            <FormLabel>Upload Images</FormLabel>
+                            <Input bg='gray.50' placeholder="Upload Images" />
+                        </Box>
+
+                        <Button w={"30%"} mt={"1%"} colorScheme="green">Submit</Button>
                     </Grid>
                 </Box>
-
             </FormControl>
 
 
