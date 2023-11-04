fced761d9e27e41916078ca10b12212e8e6ba7d8
Christian Brown <chrismbh4@gmail.com>
2023-11-03 21:10:16 -0700

    Orders seed data added

--- backend/db/seeders/20210825173739-orders.js
+++ backend/db/seeders/20210825173739-orders.js
@@ -7,7 +7,11 @@ module.exports = {
      *
      * Example:
      */
-    // await queryInterface.bulkInsert('Orders', [], {});
+    await queryInterface.bulkInsert('Orders', [
+      { username: "Demo-lition", buyerId: 1, email: "bob@gamil.com", nameOnCard: "dfasfaf", cardNumber: "sdfadsfasdfasdf", expirationDate: "01/24", cvvNumber: "sad", firstName: "sadfasdf", lastName: "fasf", company: "ASDF", address: "ASDF", otherAddress: "ASDF", city: "AF", country: "United States", stateProvince: "AL", postalCode: "78998", phoneNumber: "8234848", shoeIds: [6, 8, 11], totalAmount: "3567.3", createdAt: new Date(), updatedAt:new Date() },
+      { username: "Demo-lition", buyerId: 1, email: "bob@gmail.com", nameOnCard: "christian", cardNumber: "7887 7887 4545 8888", expirationDate: "01/24", cvvNumber: "444", firstName: "asfaf", lastName: "asdfaf", company: "", address: "dfasfsdf", otherAddress: "", city: "adaaf", country: "United States", stateProvince: "CO", postalCode: "45474", phoneNumber: "094-345-9888", shoeIds: [5, 6, 19], totalAmount: "1638.2", createdAt: new Date(), updatedAt:new Date() },
+      { username: "Demo-lition", buyerId: 1, email: "chris@gmail.com", nameOnCard: "Chrsitan Brown", cardNumber: "1111 1111 1111 1111", expirationDate: "04/28", cvvNumber: "222", firstName: "Christian", lastName: "Brown", company: "", address: "11111 hobday rd", otherAddress: "", city: "elk grove ", country: "United States", stateProvince: "CA", postalCode: "95695", phoneNumber: "832-344-2444", shoeIds: [7, 14, 16], totalAmount: "3112.8", createdAt: new Date(), updatedAt:new Date() }
+    ], {});
   },
 
   down: async (queryInterface, Sequelize) => {
@@ -16,6 +20,6 @@ module.exports = {
      *
      * Example:
      */
-    // await queryInterface.bulkDelete('Orders', null, {});
+    await queryInterface.bulkDelete('Orders', null, {});
   }
 };
